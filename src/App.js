2e6ce6c8783af40d8ce8b45f29ccc5e3bca2a2d3
Ali C. Agayev <Ali.Agayev@kapitalbank.az>
2020-12-31 23:01:01 +0400

    Change travis file.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>byy lkjl</p>
+        <p>Hello world!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
